b8e4374e406d359d191559ee47a87e0c4688daa2
Divraj-M <138122681+Divraj-M@users.noreply.github.com>
2025-02-07 10:22:27 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+    	File file=new File("input.txt");
+    	Scanner s=new Scanner(file);
+    	int size=s.nextInt();
+    	int arr[]=new int[size];
+    	
+    	for(int i=0;i<size;i++)
+    	{
+    		arr[i]=s.nextInt();
+    	}
+    	
+        
+        int max = arr[0];
+        int min = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum value: " + max);
+        System.out.println("Minimum value: " + min);
+    }
+}
